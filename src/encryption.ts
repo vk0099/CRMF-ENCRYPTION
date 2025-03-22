d19b9fb054d612b12f44ea5902b8f1131ed7b846
vk0099 <vamsikrishnagonuguntla77@gmail.com>
2025-03-22 17:14:47 +0530

    fix encryption code

--- src/encryption.ts
+++ src/encryption.ts
@@ -4,6 +4,8 @@ import Base64 from "crypto-js/enc-base64";
 import Utf8 from "crypto-js/enc-utf8";
 import Pkcs7 from "crypto-js/pad-pkcs7";
 import Rp from "sha.js";
+import bcrypt from "bcryptjs";
+import CryptoJS from "crypto-js";
 
 export default class EncryptionUtils {
   Encryptionkey = "aGgRTenEUgoACtcOAr";
@@ -17,34 +19,43 @@ export default class EncryptionUtils {
   Encrypted: string = "";
 
   Dencrypted: string = "";
+  aesKey = "a24b23c22d21e20f19g18f17e16d15c14";
+  iv = "1234567890abcdef";
 
-  iv: any = Utf8.parse("globalaesvectors");
+  // iv: any = Utf8.parse("globalaesvectors");
 
   encryption(e: string, t: string): string {
-    this.hashstring = Rp("sha256").update(this.Encryptionkey).digest("hex");
-    this.hashsubstring = this.hashstring.substring(0, 32);
-    const n = Utf8.parse(this.hashsubstring);
+    // this.hashstring = Rp("sha256").update(this.Encryptionkey).digest("hex");
+    // this.hashsubstring = this.hashstring.substring(0, 32);
+    // const n = Utf8.parse(this.hashsubstring);
     if (e === "Encrypt") {
-      const l = AES.encrypt(Utf8.parse(t), n, {
-        keySize: 16,
-        iv: this.iv,
-        mode: CryptoJSCore.mode.CBC,
-        padding: Pkcs7
+      // Decode base64 key and IV
+      const keyBytes = CryptoJS.enc.Base64.parse(this.aesKey);
+      const ivBytes = CryptoJS.enc.Base64.parse(this.iv);
+
+      // Encrypt data
+      const encrypted = CryptoJS.AES.encrypt(t, keyBytes, {
+        iv: ivBytes,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
       });
-      return l.ciphertext
-        .toString(Base64)
-        .split("+")
-        .join("-")
-        .split("/")
-        .join("_");
+
+      return encrypted.toString(); // Encrypted string (Base64)
     }
-    const i = t.split("-").join("+").split("_").join("/");
-    return AES.decrypt(i, n, {
-      keySize: 16,
-      iv: this.iv,
-      mode: CryptoJSCore.mode.CBC,
-      padding: Pkcs7
-    }).toString(Utf8);
+    const keyBytes = CryptoJS.enc.Base64.parse(this.aesKey);
+    const ivBytes = CryptoJS.enc.Base64.parse(this.iv);
+    console.log({t})
+
+    const decrypted = CryptoJS.AES.decrypt(t, keyBytes, {
+      iv: ivBytes,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+
+    const decryptedData = decrypted.toString(CryptoJS.enc.Utf8);
+    console.log({decryptedData})
+
+    return decryptedData;
   }
 
   encrypt(payload: string) {
@@ -52,6 +63,7 @@ export default class EncryptionUtils {
   }
 
   decrypt(payload: string) {
+    // console.log(payload)
     return this.encryption("Decrypt", payload);
   }
 }
